cccb07c1ccfc77e77d587c95f7725ff8f750649f
Alois Sečkár <ellrohir@seznam.cz>
2016-03-08 10:44:28 +0100

    Update elrh_dbconnect.php
    hide connection data from public

--- web/scripts/elrh_dbconnect.php
+++ web/scripts/elrh_dbconnect.php
@@ -3,9 +3,9 @@
 // these are values for homepage hosted on Wedos.cz
 // set 2015-11-09
 class ELRHDBConnectionStrings {
-	var $ELRH_MySQL_Host = "wm95.wedos.net";
-	var $ELRH_MySQL_User = "w115172_seckar";  
-	var $ELRH_MySQL_Pass = "arMsUJXq";
-	var $ELRH_MySQL_Db   = "d115172_seckar";
+	var $ELRH_MySQL_Host = "---";
+	var $ELRH_MySQL_User = "---";  
+	var $ELRH_MySQL_Pass = "---";
+	var $ELRH_MySQL_Db   = "---";
 }
 ?>
